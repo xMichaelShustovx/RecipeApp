915d0543c2ffd6020138823c1a9c1925ae199017
Michael Shustov <shustovmm@gmail.com>
2021-08-08 11:28:16 +0300

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Michael Shustov on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
